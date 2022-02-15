821fe779c50f94fdb110e4f0bae5371c94b709b9
nathanSamosa <nathanthesamosa@gmail.com>
2022-02-15 11:02:17 +0000

    req 1-3

--- src/router.js
+++ src/router.js
@@ -12,13 +12,40 @@ const mockUser = {
         age: 43
     }
 };
+const secret = "mysecretkey";
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body
+    if(mockUser.username == username && mockUser.password == password) {
+        const token = jwt.sign(
+            { username: mockUser.username },
+            secret
+        );
+        
+        console.log("LOGIN TOKEN:", token)
+        res.json({ data: token })
+    } else {
+        console.log("ERROR: username or password was incorrect")
+        res.end();
+    }
+    
 
 });
 
-router.get('/profile', (req, res) => {
+const verifyToken = token => {
+    try {
+        const decoded = jwt.verify(token, secret)
+        return decoded
 
+    } catch (err) {
+        console.log("ERROR:", err)
+        return false
+    }
+}
+
+router.get('/profile=:token', (req, res) => {
+    const { token } = req.params
+    if (verifyToken(token)) { console.log("signed in as:", mockUser) }  
 });
 
 module.exports = router;
